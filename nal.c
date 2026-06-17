5e4bb5dddcbefc576b9c379a8bbc2096fd5b457c
felipe643 <felipedlcdsantos@gmail.com>
2026-06-17 11:23:59 -0300

    teste de janela

--- nal.c
+++ nal.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
-
+#include <X11/Xlib.h>
 
 int main(){
-	pid_t pid;
+/*	pid_t pid;
 
 	printf("%d \n", getpid());
 
@@ -16,5 +16,41 @@ int main(){
 	}
 
 	printf("saindo pid = %d\n", getpid());
+
+*/
+
+	Display *dpy;
+
+	Window win;
+
+	XEvent even;
+
+	dpy = XOpenDisplay(NULL);
+
+	if (dpy == NULL) {
+        	printf("Nao foi possivel abrir o display.\n");
+        	return 1;
+	}
+
+	int screen = DefaultScreen(dpy);
+
+	win = XCreateSimpleWindow(dpy, RootWindow(dpy, screen), 100, 150/*posição*/, 800, 800/*largura*/, 2/*borda*/, BlackPixel(dpy, screen), WhitePixel(dpy, screen));
+
+	XStoreName(dpy, win, "Minha Janela");
+
+	XSelectInput(dpy, win, ExposureMask | KeyPressMask);
+
+	XMapWindow(dpy, win);
+
+	while (1) {
+        	XNextEvent(dpy, &even);
+
+        	if (even.type == KeyPress) {
+            		break;  // fecha ao pressionar uma tecla
+        	}
+    	}
+
+	XDestroyWindow(dpy, win);
+    	XCloseDisplay(dpy);
 
 }
